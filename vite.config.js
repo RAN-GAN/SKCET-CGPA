3552ed6bbbdf03f4c5837afaf116f28df4334be3
Pradeep Chandran <pradeepchandranm@gmail.com>
2025-02-17 13:51:23 +0530

    test

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/SKCET-CGPA/", // Specify your GitHub Pages URL path
+  base: "/SKCET-CGPA/", 
   plugins: [react()],
   build: {
     assetsInlineLimit: 0,
