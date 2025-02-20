3b3b74e1fd8bca80fca205e7de1e4875a5b51fd0
Pradeep Chandran <pradeepchandranm@gmail.com>
2025-02-20 18:49:46 +0530

    removed logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 function Header() {
   return (
     <header>
-      <a target="_blank" href="http://skcet.ac.in">
+      {/* <a target="_blank" href="http://skcet.ac.in">
         <img src="logo.png" alt="SKCET LOGO" />
-      </a>
+      </a> */}
+      <h1 className="headerText">
+        <span>SKCET SGPA Calculator</span>
+      </h1>
     </header>
   );
 }
