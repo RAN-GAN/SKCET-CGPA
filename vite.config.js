83bdbcfdc918d1431572f04fabfef423c87b2cdd
Pradeep Chandran <pradeepchandranm@gmail.com>
2025-06-13 14:54:35 +0530

    deploy changes

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: "/SKCET-CGPA/", 
-  plugins: [react()],
-  build: {
-    assetsInlineLimit: 0,
-    rollupOptions: {
-      input: {
-        main: "./index.html",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), ""); // load all env vars
+
+  // console.log("VITE_BASE_API:", env.VITE_BASE_API); // optional debug
+
+  return {
+    base: "/SKCET-CGPA/",
+    plugins: [react()],
+    build: {
+      assetsInlineLimit: 0,
+      rollupOptions: {
+        input: {
+          main: "./index.html",
+        },
       },
     },
-  },
-  assetsInclude: ["assets/**"], // Include assets directory
+    assetsInclude: ["assets/**"],
+  };
 });
