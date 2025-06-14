aae023d912fa8f54d444100dbfc618ee7b86498a
Pradeep Chandran <pradeepchandranm@gmail.com>
2025-06-14 15:00:40 +0530

    deploy changes to config and workflow

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,17 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), ""); // load all env vars
-
-  // console.log("VITE_BASE_API:", env.VITE_BASE_API); // optional debug
-
   return {
     base: "/SKCET-CGPA/",
     plugins: [react()],
     build: {
+      minify: "terser",
+      terserOptions: {
+        compress: {
+          drop_console: true, 
+          drop_debugger: true, 
+        },
+      },
       assetsInlineLimit: 0,
       rollupOptions: {
         input: {
